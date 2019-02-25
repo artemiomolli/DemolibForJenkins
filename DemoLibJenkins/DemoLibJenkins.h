cb0f3f05ee956af68ee833385a7e7b2ddde6e5c5
Artem Guralnyk <artem.guralnyk@smartexe.com>
2019-02-25 15:25:34 +0200

    Initial Commit

---
+++ DemoLibJenkins/DemoLibJenkins.h
@@ -0,0 +1,19 @@
+//
+//  DemoLibJenkins.h
+//  DemoLibJenkins
+//
+//  Created by Артём Гуральник on 2/25/19.
+//  Copyright © 2019 Артём Гуральник. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DemoLibJenkins.
+FOUNDATION_EXPORT double DemoLibJenkinsVersionNumber;
+
+//! Project version string for DemoLibJenkins.
+FOUNDATION_EXPORT const unsigned char DemoLibJenkinsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoLibJenkins/PublicHeader.h>
+
+
